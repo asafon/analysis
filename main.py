9fb6da74c9aeb00fa850be1751eaf2ca68e490cf
my name <me@example.com>
2025-07-12 18:01:22 +0300

    completed divide function

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ def difference(numbers):
     first = numbers[0]
     rest = numbers[1:]
     return first - sum(rest)
+
+def divide(numbers):
+    first = numbers[0]
+    rest = numbers[1:]
+    return first/sum(rest)
